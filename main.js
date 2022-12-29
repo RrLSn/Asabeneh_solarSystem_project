40c999a655cf9d9a59b1b80ceab15cfb3c2f0190
RrLSn <afolabisodiqtemitope@yahoo.com>
2022-12-29 02:05:26 +0100

    solar

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
-// selctores
-const bodyEl = document.querySelector('body')
+const planets = ['MERCURY', 'VENUS', 'EARTH', 'MARS', 'JUPITER', 'SATURN', 'URANUS', 'NEPTUNE', 'PLUTO']
+
+// selctors
+const inputEl = document.querySelector('input')
+const selectEl = document.querySelector('select')
+const buttonEl = document.querySelector('button')
+const requiredEl = document.querySelector('.required')
+const image_wrapper = document.querySelector('.img_wrapper')
+const planetEl = document.querySelector('.planet')
+const weigthEl = document.querySelector('.weigth')
