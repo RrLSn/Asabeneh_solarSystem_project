6511b2d67efb8f006067d76a42579e6300d846c2
RrLSn <afolabisodiqtemitope@yahoo.com>
2022-12-30 02:27:48 +0100

    solar

--- main.js
+++ main.js
@@ -1,17 +1,23 @@
 const planets = ['MOON','MERCURY', 'VENUS', 'EARTH', 'MARS', 'JUPITER', 'SATURN', 'URANUS', 'NEPTUNE', 'PLUTO']
 
+
 // selctors
+const bodyEl = document.querySelector('body')
 const inputEl = document.querySelector('input')
 const selectEl = document.querySelector('select')
 const buttonEl = document.querySelector('button')
 const requiredEl = document.querySelector('.required')
 const image_wrapper = document.querySelector('.img_wrapper')
+const weigth_wrapper = document.querySelector('.weigth_wrapper')
 const weigthEl = document.querySelector('.weigth_info')
 const planetEl = document.querySelector('.planet')
-// const weigthEl = document.querySelector('.weigth')
+const eachweight = document.querySelector('.weigth')
+const eachinfo = document.querySelector('.info')
 
-const activePlanet = planets[3]
-// const gravityArray = [1.6, 3.7, 8.87, 9.8, 3.7, 24.5, 10.44, 8.87, 11.15, 0.58]
+bodyEl.style.overflow = 'hidden'
+
+let activePlanet = planets[3]
+const gravityArray = [1.6, 3.7, 8.87, 9.8, 3.7, 24.5, 10.44, 8.87, 11.15, 0.58]
 
 image_wrapper.innerHTML = `<img src="./media/Earth.png" alt="planet" />`
 
@@ -33,10 +39,16 @@ buttonEl.addEventListener(
             requiredEl.textContent = 'Select a Planet'
         }else{
             activePlanet = selectEl.value;
-            // planetEl.textContent = activePlanet
-            // image_wrapper.innerHTML= `<img src="./media/${activePlanet.substr(1,activePlanet.length).toLowerCase()}.png" alt="planet" />`
-            // image_wrapper.style.display = 'block'
-            // weigthEl.style.display = 'block'
+            planetEl.textContent = activePlanet
+            image_wrapper.innerHTML= `<img src="./media/${activePlanet}.png" alt="planet" />`
+            image_wrapper.style.display = 'block'
+            weigthEl.style.display = 'block'
+            requiredEl.style.display = 'none'
+            weigth_wrapper.style.display = 'block'
+            let i = planets.indexOf(selectEl.value)
+            let answer = gravityArray[i] * inputEl.value
+            eachweight.textContent = `${answer.toFixed(2)} N`
+            eachweight.style.margin = '1.5rem auto'
         }
     }
 )
